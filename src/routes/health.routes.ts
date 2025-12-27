2e2ee6f38e39f454b58aff3c6097c6a25984f72a
Mirza Muhammad Danish Baig <baigmirzadanish3@gmail.com>
2025-12-27 18:10:08 +0500

    refactor: attach request-id middleware to health endpoint

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getHealthStatus } from "@/controllers/health.controller";
+import { requestIdMiddleware } from "@/middlewares/request-id.middleware";
 
 const router = Router();
 
 // GET /api/health
+router.use(requestIdMiddleware)
 router.get("/", getHealthStatus);
 
 export default router;
