f7f91f318bc4b0c45b955685013f9e5db9f973d5
Mirza Muhammad Danish Baig <baigmirzadanish3@gmail.com>
2025-12-27 16:23:24 +0500

    refactor: request-id middleware

--- src/middlewares/request-id.middleware.ts
+++ src/middlewares/request-id.middleware.ts
@@ -1,3 +1,4 @@
+import { logger } from "@/utils/logger";
 import { randomUUID } from "crypto";
 import { Response, Request, NextFunction } from "express";
 
@@ -7,11 +8,34 @@ export const requestIdMiddleware = (
   next: NextFunction
 ) => {
   const requestId = randomUUID();
+  const start = Date.now();
 
   req.requestId = requestId;
   res.setHeader("X-Request-ID", requestId);
+
+  logger.info({
+    requestId,
+    method: req.method,
+    path: req.path,
+    ip: req.ip,
+    userAgent: req.get("user-agent"),
+    msg: "Request started",
+  });
+
+  res.on("finish", () => {
+    logger.info({
+      requestId,
+      method: req.method,
+      path: req.path,
+      statusCode: res.statusCode,
+      durationMs: Date.now() - start,
+      msg: "Request completed",
+    });
+  });
+
   next();
 };
+
 
 declare global {
   namespace Express {
